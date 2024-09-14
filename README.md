df5a16712e80cf65175e02cc361d6838027a4ae7
urmila1907 <urmilasangwan744@gmail.com>
2024-09-15 01:11:44 +0530

    Added more styling

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# lottery_ticket
+# Lottery Ticket
+
+Changing react states effectively and correctly. Dynamic layout for 2 games.
+Play, Win and have fun!
--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
     <div className="App">
         <div className='main-ball'>
+          <div className='main-text'> Main Game</div>
           <MainLottoBall num={6}/>
         </div>
         <div className='mini-ball'>
+        <div className='mini-text'> Mini Game</div>
           <MainLottoBall num={4}/>
         </div>
     </div>
